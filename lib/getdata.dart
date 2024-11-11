2df31804d1ee4d36996ba160b063afb439799017
JNakum <jayeshekika@gmail.com>
2024-11-11 11:33:44 +0530

    change getdatafile

--- lib/getdata.dart
+++ lib/getdata.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:intl/intl.dart';
 
+// ignore: must_be_immutable
 class Getdata extends StatefulWidget {
   Getdata(
       {super.key,
